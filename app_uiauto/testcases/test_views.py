0fcee1ab6369168a596807fe4d82316f2ec9f1c4
sandy <sandy@test.com>
2020-08-08 11:46:25 +0800

    练习使用pytest.main()方式来运行case，已跑通，但是注释掉

--- app_uiauto/testcases/test_views.py
+++ app_uiauto/testcases/test_views.py
@@ -1,18 +1,23 @@
+import pytest
+
 from app_uiauto.page import Views
 from app_uiauto.page.app import App
 
 
 class TestViews:
     def setup(self):
-        self.app=App()
-        self.views=self.app.start().main().goto_view()
+        self.app = App()
+        self.views = self.app.start().main().goto_view()
 
     def test_popmenu(self):
-        self.toast_text=self.views.popmenu()
+        self.toast_text = self.views.popmenu()
         # 获取toastText来断言是否正确
-        assert self.toast_text=="Clicked popup menu item Search"
+        assert self.toast_text == "Clicked popup menu item Search"
 
     def test_webview(self):
         self.views.into_webview()
 
 
+# 需要配置python解释器环境，来跑这个python运行入口程序
+# if __name__ == "__main__":
+#     pytest.main(['-vs','test_views.py::TestViews::test_popmenu'])
